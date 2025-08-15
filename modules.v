a63e4af370375763e4dd7e3d2bdfeb49709f0ff5
Anirudh Kanwar <anirudhkanwar5@gmail.com>
2025-08-15 15:28:49 +0530

    Update modules.v

--- modules.v
+++ modules.v
@@ -1,193 +1,120 @@
-module tfsync #(parameter WIDTH=3)(
-      input [WIDTH:0] din,
-      input clk,
-      input rst,
-      output reg [WIDTH:0] dout
-);
-reg[WIDTH:0] dmeta;
+timescale 1ns / 1ps
 
-always @(posedge clk or negedge rst)
-begin
-    if(!rst)
-    begin
-        dmeta<=0;
-        dout<=0;
-    end
 
-    else
-    begin
-        dmeta<=din;
-        dout<=dmeta;
-    end
-end
-    
-endmodule
-
-/*
-module b2g_convert #(
-    parameter PTR_WIDTH=3
-) (
-    input [PTR_WIDTH-1:0] binary_ptr,
-
-    output reg [PTR_WIDTH-1:0] gray_ptr
-    
-);
-
-always@(*)
-begin
-    gray_ptr<=binary_ptr^(binary_ptr>>1);
-end
-    
-endmodule
-
-module g2b_convert #(
-    parameter PTR_WIDTH=3
-) (
-    input [PTR_WIDTH-1:0] gray_input,
-    output reg [PTR_WIDTH-1:0] binary_output
-);
-integer i;
-
-always@(*)begin
-    binary_output[PTR_WIDTH-1]<=gray_input[PTR_WIDTH-1];
-    for (i =1;i<PTR_WIDTH ;i=i+1 ) begin
-        binary_output[i]<=binary_output[i-1]^(gray_input[i]);
-        
-    end
-end
-
-    
-endmodule
-*/
-
-module wptr_handler #(
-    parameter WIDTH=3
-) (
-    input wclk,wrst,w_en,
-    input [WIDTH:0] g_rptr_sync,
-    output reg [WIDTH:0] b_wptr,
-    output reg [WIDTH:0] g_wptr,
-    output reg full
-);
+module fifo(rst,ren,wen,wclk,rclk,wr_data,rd_data,full,empty,overflow,underflow);
+  input ren,wen;
+  input rst,wclk,rclk;
+  parameter fifo_width = 8;
+  parameter fifo_depth = 8;
+  parameter address_size = 3;
+  reg [address_size-1:0]rd_ptr,wr_ptr;
+  input [fifo_width-1:0]wr_data;
+  output reg [fifo_width-1:0]rd_data;
+  output reg full;
+  output reg empty;
+  output reg overflow;
+  output reg underflow;
+  reg [address_size:0]rd_ptr_b2g;
+  reg [address_size:0]wr_ptr_b2g;
+  reg [address_size:0]rd_ptr_sy1, rd_ptr_sy2;
+  reg [address_size:0]wr_ptr_sy1, wr_ptr_sy2;
+  reg [address_size:0]wr_ptr_sy2_g2b,rd_ptr_sy2_g2b;
+  reg [fifo_width-1:0]mem[fifo_depth-1:0];
+  
  
-wire [WIDTH:0] b_wptr_nxt;
-wire [WIDTH:0] g_wptr_nxt;
-wire w_full;
-assign b_wptr_nxt=b_wptr+(w_en&!full);
-assign g_wptr_nxt=b_wptr_nxt^(b_wptr_nxt>>1);
-
-always@(posedge wclk or negedge wrst)begin
-    if(!wrst)begin
-        b_wptr<=0;
-        g_wptr<=0;
+  always @(*)
+  begin
+    if(rst) begin 
+    rd_ptr<=0;
+    wr_ptr<=0;
+    underflow<=0;
+     overflow<=0;
+     full <=0;
+     empty <=1;
     end
-
-    else begin
-        b_wptr<=b_wptr_nxt;
-        g_wptr<=g_wptr_nxt;
+    
     end
-end
-assign w_full=(g_wptr_nxt==({~(g_rptr_sync[WIDTH:WIDTH-1]),g_rptr_sync[WIDTH-2:0]}));
-always @(posedge wclk or negedge wrst) begin
-    if(!wrst) full<=0;
-    else full<=w_full;
+  always @(posedge wclk)
+    begin
+          if(wen && !full)
+          begin
+          mem[wr_ptr]<=wr_data;
+            wr_ptr<= wr_ptr+1;
+           
+            end
+    end
+  
+  always @(posedge rclk)
+    begin
+          if(ren && !empty)
+          begin
+          rd_data<= mem[rd_ptr];
+        rd_ptr <= rd_ptr+1;
+          end
+       
+    end    
+  
+  always @(posedge rclk )
+  begin
+    empty<=(wr_ptr_sy2_g2b[address_size:0] == rd_ptr[address_size:0])?1:0;
+       end
+     
+  
+  always @(posedge wclk)
+  begin
+ 
+    full<=({~wr_ptr[address_size],wr_ptr[address_size-1:0]} == rd_ptr_sy2_g2b[address_size:0])?1:0;
       
-end
-
-    
-endmodule
-
-
-module rptr_handler #(
-    parameter WIDTH=3
-) (
-    input rclk,rrst,r_en,
-    input [WIDTH:0] g_wptr_sync,
-    output reg [WIDTH:0] b_rptr,
-    output reg [WIDTH:0] g_rptr,
-    output reg  empty
-);
-wire [WIDTH:0] b_rptr_nxt,g_rptr_nxt;
-wire r_emp;
-
-assign b_rptr_nxt=b_rptr+(r_en & !empty);
-assign g_rptr_nxt=b_rptr_nxt^(b_rptr_nxt>>1);
-assign r_emp=(g_wptr_sync==g_rptr_nxt);
-
-always @(posedge rclk or negedge rrst) begin
-     if(!rrst)begin
-        b_rptr<=0;
-        g_rptr=0;
      end
-
-     else begin
-        b_rptr<=b_rptr_nxt;
-        g_rptr<=g_rptr_nxt;
-     end
-end
-always @(posedge rclk or negedge rrst) begin
-    if(!rrst)begin
-            empty<=1;
-    end
-
-    else empty<=r_emp;
-    
-end
-endmodule
-
-module fifo #(
-    parameter DEPTH=8,DATA_WIDTH=8,PTR_WIDTH=3
-) (
-    input w_clk,w_en,rclk,r_en,
-    input [PTR_WIDTH:0] b_wptr,b_rptr,
-    input [DATA_WIDTH-1:0] data_in,
-    input full,empty,
-    output reg [DATA_WIDTH-1:0] data_out
-);
-
-reg[DATA_WIDTH-1:0] FIFO [0:DEPTH-1];
-
-always @(posedge w_clk) begin
-    if(w_en & !full)begin
-        FIFO[b_wptr[PTR_WIDTH-1:0]]<=data_in;
-    end
-    
-end
-
-always @(posedge rclk) begin
-
-    if (r_en & !empty) begin
-
-        data_out<=FIFO[b_rptr[PTR_WIDTH-1:0]];
-        
-    end
-    
-end
-endmodule
-
-
-
-module asynchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8,PTR_WIDTH=3) (
-  input wclk, wrst_n,
-  input rclk, rrst_n,
-  input w_en, r_en,
-  input [DATA_WIDTH-1:0] data_in,
-  output [DATA_WIDTH-1:0] data_out,
-  output full, empty
-);
-  
-  
  
-  wire [PTR_WIDTH:0] g_wptr_sync, g_rptr_sync;
-  wire [PTR_WIDTH:0] b_wptr, b_rptr;
-  wire [PTR_WIDTH:0] g_wptr, g_rptr;
+  always @(posedge rclk )
+    begin 
+      if(ren && empty)
+        underflow <=1;
+      else
+        underflow<=0;
+    end
   
+  always @(posedge wclk or posedge rst)
+    begin 
+      if(wen && full)
+        overflow <=1;
+      else
+        overflow<=0;
+    end
 
-  tfsync #(PTR_WIDTH) sync_wptr (g_wptr,wclk,wrst_n, g_wptr_sync); //write pointer to read clock domain
-  tfsync #(PTR_WIDTH) sync_rptr (g_rptr,rclk,rrst_n, g_rptr_sync); //read pointer to write clock domain 
+  always @(*)
+    begin 
+  rd_ptr_b2g <= rd_ptr^(rd_ptr>>1);
+  wr_ptr_b2g <= wr_ptr^(wr_ptr>>1);
+    end
   
-  wptr_handler #(PTR_WIDTH) wptr_h(wclk, wrst_n, w_en,g_rptr_sync,b_wptr,g_wptr,full);
-  rptr_handler #(PTR_WIDTH) rptr_h(rclk, rrst_n, r_en,g_wptr_sync,b_rptr,g_rptr,empty);
-  fifo #(DEPTH,DATA_WIDTH,PTR_WIDTH)fifom(wclk, w_en, rclk, r_en,b_wptr, b_rptr, data_in,full,empty,data_out);
+  always @(posedge wclk)
+    begin 
+      rd_ptr_sy1 <= rd_ptr_b2g;
+      rd_ptr_sy2 <= rd_ptr_sy1;
+    end
+  
+  always @(posedge rclk)
+    begin 
+      wr_ptr_sy1 <= wr_ptr_b2g;
+      wr_ptr_sy2 <= wr_ptr_sy1;
+    end
+  integer i;
+  always @(*)
+        begin 
+      wr_ptr_sy2_g2b[address_size] <= wr_ptr_sy2[address_size];
+      
+      for( i = address_size - 1 ; i>=0;i=i-1)
+        wr_ptr_sy2_g2b[i] <= wr_ptr_sy2_g2b[i+1]^wr_ptr_sy2[i];
+        end
 
+  always @(*)
+        begin 
+      rd_ptr_sy2_g2b[address_size] <= rd_ptr_sy2[address_size];
+      
+      for(i= address_size - 1 ; i>=0;i = i-1)
+        rd_ptr_sy2_g2b[i]<= rd_ptr_sy2_g2b[i+1]^rd_ptr_sy2[i];
+       end
+  
 endmodule
