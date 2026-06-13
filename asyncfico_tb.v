04906f1e659002ba105e7e7ae11d43dd1b8c58ca
Anirudh Kanwar <anirudhkanwar5@gmail.com>
2026-06-13 22:47:23 +0530

    Update asyncfico_tb.v

--- asyncfico_tb.v
+++ asyncfico_tb.v
@@ -1,58 +1,91 @@
-`timescale 1ns / 1ps
+`include "asyncfifo.v"
+module async_fifo_TB;
+
+  parameter DATA_WIDTH = 8;
+
+  wire [DATA_WIDTH-1:0] dataout;
+  wire full;
+  wire empty;
+  reg [DATA_WIDTH-1:0] datain;
+  reg w_en, wrclk, wrst;
+  reg r_en, rclk, rrst;
 
 
-module fifo_tb();
 
+  asynchronous_fifo asf (wrclk,wrst,rclk,rrst,w_en,r_en,datain,dataout,full,empty);
+
+  always #7 wrclk = ~wrclk;
+  always #37 rclk = ~rclk;
   
-  reg ren, wen;
-  reg rst, wclk, rclk;
-  reg [7:0] wr_data; 
+  initial begin
+    wrclk = 1'b0; wrst = 1'b0;
+    w_en = 1'b0;
+    datain <= 0;
+    
+    repeat(10) @(posedge wrclk);
+    wrst = 1'b1;
 
-  wire [7:0] rd_data;
-  wire full, empty, overflow, underflow;
-
+    repeat(2) begin
+      for (integer i=0; i<30; i++) begin
+        @(posedge wrclk) 
+        if (!full) begin 
+        w_en = (i%2 == 0)? 1'b1 : 1'b0; 
+        datain = (w_en) ? $random: datain;
+        end
+       
+      end
+      end
   
-  fifo dut (
-    .rst(rst),
-    .ren(ren),
-    .wen(wen),
-    .wclk(wclk),
-    .rclk(rclk),
-    .wr_data(wr_data),
-    .rd_data(rd_data),
-    .full(full),
-    .empty(empty),
-    .overflow(overflow),
-    .underflow(underflow)
-  );
-initial begin 
-rst = 1'b1;
-ren = 1'b1;
-wen = 1'b1;
-wclk = 0;
-rclk = 0;
-wr_data = 8'b10001111;
-#50 rst = 1'b0;
-#60 wr_data = 8'b11111111;
-#100 wr_data = 8'b11111001;
-#150 wr_data = 8'b11100001;
-#200 wr_data = 8'b11111001;
-#250 wr_data = 8'b01111001;
-#300 wr_data = 8'b00111001;
-#400 wr_data = 8'b11111011;
- #1000 $stop;
- end
+    
+  end
 
-always begin 
-#10 wclk = ~wclk;
+  initial begin
+    rclk = 1'b0; rrst = 1'b0;
+    r_en = 1'b0;
+
+    repeat(20) @(posedge rclk);
+    rrst = 1'b1;
+
+    repeat(2) begin
+      for (integer i=0; i<30; i++) begin
+        @(posedge rclk) 
+        if (!empty) begin
+        r_en = (i%2 == 0)? 1'b1 : 1'b0;
+     end
+      #50;
+    end
+
+    repeat(2) begin
+      for (integer i=0; i<30; i++) begin
+        @(posedge wrclk) 
+        if (!full) begin 
+        w_en = (i%2 == 0)? 1'b1 : 1'b0;
+        datain = (w_en) ? $random: datain; 
+        end
+        
+      end
+      end
+
+      repeat(2) begin
+      for (integer i=0; i<30; i++) begin
+        @(posedge rclk) 
+        if (!empty) begin
+        r_en = (i%2 == 0)? 1'b1 : 1'b0;
+     end
+      #50;
+    end
+
+   
+
+    
+  end
+  
+#8000 $finish;
+end 
 end
-always begin
-#20 rclk = ~rclk;
-end
-initial 
+initial
 begin
-  $dumpfile("dump.vcd");
-$dumpvars;
-
+$dumpfile("dcfifo.vcd");
+$dumpvars(0,async_fifo_TB);
 end
 endmodule
